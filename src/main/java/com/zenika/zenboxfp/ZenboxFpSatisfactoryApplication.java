1bdb6625622df2cc7031040451f4a33a2950b980
Hugo Wood <hx4.5@free.fr>
2019-06-13 13:02:25 +0200

    :tada: spring boot initializer with web

---
+++ src/main/java/com/zenika/zenboxfp/ZenboxFpSatisfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.zenboxfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenboxFpSatisfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenboxFpSatisfactoryApplication.class, args);
+	}
+
+}
